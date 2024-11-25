568d474a3bf125be08969be04bb03bac07bf5a9b
izaelgs <izael-123@outlook.com>
2024-11-25 15:58:09 -0300

    ajustes

---
+++ src/components/MultipleDropzoneComponent.tsx
@@ -0,0 +1,91 @@
+import 'react-dropzone-uploader/dist/styles.css';
+import { useRef, useCallback } from 'react';
+import Dropzone, { defaultClassNames, IFileWithMeta, StatusValue } from 'react-dropzone-uploader';
+
+import DropzonePreview from '../components/dropzone/DropzonePreview';
+import { notify } from '../components/toast/NotificationIcon';
+import api, { apiUrl } from '../services/useAxios';
+
+interface MultipleDropzoneComponentProps {
+  endpoint: string;
+  placeholder?: string;
+  maxFiles?: number;
+  accept?: string;
+  maxSizeBytes?: number;
+  onChange: (_value: IFileWithMeta[]) => void; // Alterado para retornar IFileWithMeta[]
+  onRemove?: (response: string) => void;
+}
+
+const MultipleDropzoneComponent = (
+  ({ endpoint, placeholder, maxFiles = 20, accept = 'image/*', maxSizeBytes = 16 * 1024 * 1024, onChange, onRemove }: MultipleDropzoneComponentProps) => {
+    const dropzoneRef = useRef<Dropzone>(null);
+    const filesRef = useRef<IFileWithMeta[]>([]);
+
+    // Function to handle changes in file status
+    const handleChangeStatus = useCallback(
+      (file: IFileWithMeta, status: StatusValue, allFiles: IFileWithMeta[]) => {
+        // Set the current files in the ref
+        filesRef.current = allFiles;
+
+        // Notify parent with the updated files, sending all files
+        onChange(allFiles); // Agora retorna todos os arquivos IFileWithMeta[]
+
+        // Handle specific statuses for error messages or file removal
+        switch (status) {
+          case 'error_validation':
+            file.remove();
+            break;
+          case 'rejected_file_type':
+            notify('Tipo de arquivo inválido.', 'Erro', 'close', 'danger');
+            break;
+          case 'error_file_size':
+            notify('Tamanho de arquivo excede o limite permitido.', 'Erro', 'close', 'danger');
+            file.remove();
+            break;
+          case 'rejected_max_files':
+            notify(`Não é possível enviar mais de ${maxFiles} arquivos!`, 'Erro', 'close', 'danger');
+            file.remove();
+            break;
+          case 'removed':
+            if (file.xhr?.response) {
+              api.delete(endpoint, { data: { url: file.xhr.response } });
+              onRemove?.(file.xhr.response); // Notify the parent that a file was removed
+            } else {
+              console.error('Erro ao remover arquivo');
+            }
+            break;
+          case 'error_upload':
+            notify('Erro ao enviar arquivo', 'Erro', 'close', 'danger');
+            file.remove();
+            break;
+          default:
+            break;
+        }
+      },
+      [endpoint, maxFiles, onChange, onRemove]
+    );
+
+    const getUploadParams = () => ({
+      url: apiUrl + endpoint,
+    });
+
+    return (
+      <Dropzone
+        ref={dropzoneRef}
+        submitButtonContent={null}
+        onChangeStatus={handleChangeStatus} // Handle status changes here
+        PreviewComponent={DropzonePreview}
+        getUploadParams={getUploadParams}
+        submitButtonDisabled
+        maxFiles={maxFiles}
+        accept={accept}
+        maxSizeBytes={maxSizeBytes}
+        inputWithFilesContent={null}
+        classNames={{ inputLabelWithFiles: defaultClassNames.inputLabel }}
+        inputContent={placeholder ? placeholder : 'Clique aqui para selecionar os arquivos'}
+      />
+    );
+  }
+);
+
+export default MultipleDropzoneComponent;
--- src/pages/Dashboard/AdvertisementForm.tsx
+++ src/pages/Dashboard/AdvertisementForm.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Col, Form, Row, Card } from 'react-bootstrap';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import { useQueryClient } from '@tanstack/react-query';
+import { IFileWithMeta } from 'react-dropzone-uploader';
+import { notEmpty } from '../../helpers/Utils';
+import CsLineIcons from '../../cs-line-icons/CsLineIcons';
+import MultipleDropzoneComponent from '../../components/MultipleDropzoneComponent';
+import AsyncButton from '../../components/AsyncButton';
 
 interface FormValues {
   advertiser_id: string;
@@ -17,10 +23,21 @@ interface FormValues {
   categories: string[];
   public: string[];
   traits: string[];
+  photos: Record<string, string>;
+  videos: Record<string, string>;
 }
 
 const AdvertisementForm: React.FC = () => {
+  const queryClient = useQueryClient();
+
+  const [isSaving, setIsSaving] = useState(false);
+  const [imageFiles, setImageFiles] = useState<IFileWithMeta[]>([]);
+  const [videoFiles, setVideoFiles] = useState<IFileWithMeta[]>([]);
+  const [isRemovingPhotoUrls, setIsRemovingPhotoUrls] = useState<string[]>([]);
+  const [isRemovingVideoUrls, setIsRemovingVideoUrls] = useState<string[]>([]);
+
   const initialValues: FormValues = {
+    name: '',
     advertiser_id: '',
     state: '',
     city: '',
@@ -32,7 +49,41 @@ const AdvertisementForm: React.FC = () => {
     locations: [],
     categories: [],
     public: [],
-    traits: []
+    traits: [],
+    photos: {
+      photo1: '',
+      photo2: '',
+      photo3: '',
+      photo4: '',
+      photo5: '',
+      photo6: '',
+      photo7: '',
+      photo8: '',
+      photo9: '',
+      photo10: '',
+      photo11: '',
+      photo12: '',
+      photo13: '',
+      photo14: '',
+      photo15: '',
+    },
+    videos: {
+      video1: '',
+      video2: '',
+      video3: '',
+      video4: '',
+      video5: '',
+      video6: '',
+      video7: '',
+      video8: '',
+      video9: '',
+      video10: '',
+      video11: '',
+      video12: '',
+      video13: '',
+      video14: '',
+      video15: '',
+    },
   };
 
   const validationSchema = Yup.object().shape({
@@ -47,10 +98,129 @@ const AdvertisementForm: React.FC = () => {
     categories: Yup.array().required('Categorias são obrigatórias'),
     public: Yup.array().required('Público é obrigatório'),
     traits: Yup.array().required('Traços é obrigatório'),
+    photos: Yup.object().required('Fotos são obrigatórias'),
   });
 
   const onSubmit = (values: FormValues) => {
     console.log('Form submitted', values);
+  };
+
+  const handleChangeMultiplePhotos = (files: IFileWithMeta[]) => {
+    setImageFiles(files);
+
+    const filesUrls = files.map((file) => file.xhr?.response).filter((url: string | undefined) => url?.length);
+
+    const photoUrls = Object.values(values.photos).filter((url) => url?.length);
+
+    const combinedUrls = [...photoUrls, ...filesUrls].filter(notEmpty).reduce((acc: string[], url) => {
+      if (url && !acc.includes(url)) acc.push(url);
+      return acc;
+    }, []);
+
+    Object.keys(values.photos).forEach((key, index) => {
+      formik.setFieldValue(`photos.${key}`, combinedUrls[index] ?? '');
+    });
+  };
+
+  const handleChangeMultipleVideos = (files: IFileWithMeta[]) => {
+    setVideoFiles(files);
+
+    const filesUrls = files.map((file) => file.xhr?.response).filter((url: string | undefined) => url?.length);
+
+    const videoUrls = Object.values(values.videos).filter((url) => url?.length);
+
+    const combinedUrls = [...videoUrls, ...filesUrls].filter(notEmpty).reduce((acc: string[], url) => {
+      if (url && !acc.includes(url)) acc.push(url);
+      return acc;
+    }, []);
+
+    Object.keys(values.videos).forEach((key, index) => {
+      formik.setFieldValue(`videos.${key}`, combinedUrls[index] ?? '');
+    });
+  };
+
+  const handleClickRemovePhoto = async (response: string) => {
+    try {
+      const photoKey = Object.keys(values.photos).find((key) => values.photos[key] == response);
+
+      const file = imageFiles.find((file) => file.xhr?.response == response);
+
+      if (file) {
+        file.remove();
+      } else if (photoKey) {
+        setIsRemovingPhotoUrls((prev) => [...prev, response]);
+
+        // if (property?.additional_information?.id) {
+        //   await updatePropertyAdditionalIinformation(
+        //     property.additional_information.id,
+        //     { photos: { ...values.photos, [photoKey]: '' } },
+        //     queryClient
+        //   );
+        // }
+
+        formik.setFieldValue(`photos.${photoKey}`, '');
+        setIsRemovingPhotoUrls((prev) => prev.filter((key) => key !== response));
+      }
+    } catch (error) {
+      console.error('Error removing photo:', error);
+    }
+  };
+
+  const handleClickRemoveVideo = async (response: string) => {
+    try {
+      const videoKey = Object.keys(values.videos).find((key) => values.videos[key] == response);
+
+      const file = videoFiles.find((file) => file.xhr?.response == response);
+
+      if (file) {
+        file.remove();
+      } else if (videoKey) {
+        setIsRemovingVideoUrls((prev) => [...prev, response]);
+
+        formik.setFieldValue(`videos.${videoKey}`, '');
+        setIsRemovingVideoUrls((prev) => prev.filter((key) => key !== response));
+      }
+    } catch (error) {
+      console.error('Error removing video:', error);
+    }
+  };
+
+  const onDropzoneRemovePhoto = async (response: string) => {
+    try {
+      const photoKey = Object.keys(values.photos).find((key) => values.photos[key] === response);
+
+      if (photoKey) {
+        setIsRemovingPhotoUrls((prev) => [...prev, response]);
+
+        // if (property?.additional_information?.id) {
+        //   await updatePropertyAdditionalIinformation(
+        //     property.additional_information.id,
+        //     { photos: { ...values.photos, [photoKey]: '' } },
+        //     queryClient
+        //   );
+        // }
+
+        formik.setFieldValue(`photos.${photoKey}`, '');
+        setIsRemovingPhotoUrls((prev) => prev.filter((key) => key !== response));
+      }
+    } catch (error) {
+      console.error('Error removing photo:', error);
+    }
+  };
+
+  const onDropzoneRemoveVideo = async (response: string) => {
+    try {
+      const videoKey = Object.keys(values.videos).find((key) => values.videos[key] === response);
+
+      if (videoKey) {
+        setIsRemovingVideoUrls((prev) => [...prev, response]);
+
+        formik.setFieldValue(`videos.${videoKey}`, '');
+        setIsRemovingVideoUrls((prev) => prev.filter((key) => key !== response));
+      }
+    } catch (error) {
+      console.error('Error removing video:', error);
+    }
   };
 
   const formik = useFormik<FormValues>({ initialValues, validationSchema, onSubmit });
@@ -64,35 +234,100 @@ const AdvertisementForm: React.FC = () => {
           <Row className="mb-3 g-3">
             <Col md="12">
               <Form.Group className="form-group position-relative tooltip-end-top">
-                <Form.Label className='fw-bold'>Titulo do anúncio</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="title"
-                  onChange={handleChange}
-                  value={values.title}
-                  isInvalid={!!errors.title && touched.title}
-                />
+                <Form.Label className="fw-bold">Titulo do anúncio</Form.Label>
+                <Form.Control type="text" name="title" onChange={handleChange} value={values.title} isInvalid={!!errors.title && touched.title} />
                 <Form.Control.Feedback type="invalid">{errors.title}</Form.Control.Feedback>
               </Form.Group>
             </Col>
 
             <Col md="12">
-              <Form.Group className="form-group position-relative tooltip-end-top">
-                <Form.Label className='fw-bold'>Fotos</Form.Label>
-                <p>Dropzone do broker aqui</p>
-              </Form.Group>
+              <h5 className="fw-bold">Fotos</h5>
+              <MultipleDropzoneComponent
+                endpoint="/advertiser/image"
+                onChange={handleChangeMultiplePhotos}
+                onRemove={onDropzoneRemovePhoto}
+                maxFiles={15 - Object.values(values.photos).filter((image) => image?.length && !imageFiles.find((file) => file.xhr?.response === image)).length}
+              />
+
+              {/* Display uploaded photos */}
+              <Row className="mt-3">
+                {Object.entries(values.photos).map(
+                  ([key, value], index) =>
+                    value && (
+                      <Col md="3" key={key} className="mb-3">
+                        <Card className="border">
+                          <Card.Img variant="top" src={value} className="img-fluid sh-20" />
+                          <Card.Body className="d-flex justify-content-between align-items-center p-2">
+                            <small>{`${index + 1} de ${Object.values(values.photos).filter(Boolean).length}`}</small>
+                            <div>
+                              <a href={value} target="_blank" rel="noopener noreferrer" className="btn btn-outline-primary btn-icon btn-icon-only me-2">
+                                <CsLineIcons icon="eye" />
+                              </a>
+                              <AsyncButton
+                                loadingText=" "
+                                isSaving={isRemovingPhotoUrls.includes(value)}
+                                variant="outline-primary"
+                                onClickHandler={() => handleClickRemovePhoto(value)}
+                                className="btn-icon btn-icon-only"
+                              >
+                                <CsLineIcons icon="bin" />
+                              </AsyncButton>
+                            </div>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    )
+                )}
+              </Row>
+            </Col>
+
+            <Col md="12">
+              <h5 className="fw-bold">Vídeos</h5>
+              <MultipleDropzoneComponent
+                endpoint="/advertiser/video"
+                onChange={handleChangeMultipleVideos}
+                onRemove={onDropzoneRemoveVideo}
+                accept="video/*"
+                maxSizeBytes={200 * 1024 * 1024} // 200 MB
+                maxFiles={3 - Object.values(values.photos).filter((image) => image?.length && !imageFiles.find((file) => file.xhr?.response === image)).length}
+              />
+
+              {/* Display uploaded photos */}
+              <Row className="mt-3">
+                {Object.entries(values.videos).map(
+                  ([key, value], index) =>
+                    value && (
+                      <Col md="3" key={key} className="mb-3">
+                        <Card className="border">
+                          {/* <Card.Img variant="top" src={value} className="img-fluid sh-20" /> */}
+                          <video src={value} className="img-fluid sh-20" controls />
+                          <Card.Body className="d-flex justify-content-between align-items-center p-2">
+                            <small>{`${index + 1} de ${Object.values(values.videos).filter(Boolean).length}`}</small>
+                            <div>
+                              <a href={value} target="_blank" rel="noopener noreferrer" className="btn btn-outline-primary btn-icon btn-icon-only me-2">
+                                <CsLineIcons icon="eye" />
+                              </a>
+                              <AsyncButton
+                                loadingText=" "
+                                isSaving={isRemovingVideoUrls.includes(value)}
+                                variant="outline-primary"
+                                onClickHandler={() => handleClickRemoveVideo(value)}
+                                className="btn-icon btn-icon-only"
+                              >
+                                <CsLineIcons icon="bin" />
+                              </AsyncButton>
+                            </div>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    )
+                )}
+              </Row>
             </Col>
 
             <Col md="12">
               <Form.Group className="form-group position-relative tooltip-end-top">
-                <Form.Label className='fw-bold'>Vídeos</Form.Label>
-                <p>Dropzone do broker aqui</p>
-              </Form.Group>
-            </Col>
-
-            <Col md="12">
-              <Form.Group className="form-group position-relative tooltip-end-top">
-                <Form.Label className='fw-bold'>Descrição</Form.Label>
+                <Form.Label className="fw-bold">Descrição</Form.Label>
                 <Form.Control
                   as="textarea"
                   rows={3}
@@ -106,27 +341,15 @@ const AdvertisementForm: React.FC = () => {
             </Col>
             <Col md="6">
               <Form.Group className="form-group position-relative tooltip-end-top">
-                <Form.Label className='fw-bold'>Estado</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="state"
-                  onChange={handleChange}
-                  value={values.state}
-                  isInvalid={!!errors.state && touched.state}
-                />
+                <Form.Label className="fw-bold">Estado</Form.Label>
+                <Form.Control type="text" name="state" onChange={handleChange} value={values.state} isInvalid={!!errors.state && touched.state} />
                 <Form.Control.Feedback type="invalid">{errors.state}</Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col md="6">
               <Form.Group className="form-group position-relative tooltip-end-top">
-                <Form.Label className='fw-bold'>Cidade</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="city"
-                  onChange={handleChange}
-                  value={values.city}
-                  isInvalid={!!errors.city && touched.city}
-                />
+                <Form.Label className="fw-bold">Cidade</Form.Label>
+                <Form.Control type="text" name="city" onChange={handleChange} value={values.city} isInvalid={!!errors.city && touched.city} />
                 <Form.Control.Feedback type="invalid">{errors.city}</Form.Control.Feedback>
               </Form.Group>
             </Col>
@@ -135,11 +358,11 @@ const AdvertisementForm: React.FC = () => {
           <Row className="mb-3 g-3">
             <Col md="4">
               <Form.Group className="form-group position-relative tooltip-end-top">
-                <Form.Label className='fw-bold'>Disponibilidade de horário</Form.Label>
+                <Form.Label className="fw-bold">Disponibilidade de horário</Form.Label>
                 <Form.Control
                   type="text"
                   name="availability"
-                  placeholder='Exemplo: das 10 ás 22 ou 24h'
+                  placeholder="Exemplo: das 10 ás 22 ou 24h"
                   onChange={handleChange}
                   value={values.availability}
                   isInvalid={!!errors.availability && touched.availability}
@@ -149,27 +372,15 @@ const AdvertisementForm: React.FC = () => {
             </Col>
             <Col md="4">
               <Form.Group className="form-group position-relative tooltip-end-top">
-                <Form.Label className='fw-bold'>Valor R$ (A combinar, deixe 0)</Form.Label>
-                <Form.Control
-                  type="number"
-                  name="price"
-                  onChange={handleChange}
-                  value={values.price}
-                  isInvalid={!!errors.price && touched.price}
-                />
+                <Form.Label className="fw-bold">Valor R$ (A combinar, deixe 0)</Form.Label>
+                <Form.Control type="number" name="price" onChange={handleChange} value={values.price} isInvalid={!!errors.price && touched.price} />
                 <Form.Control.Feedback type="invalid">{errors.price}</Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col md="4">
               <Form.Group className="form-group position-relative tooltip-end-top">
-                <Form.Label className='fw-bold'>Idade</Form.Label>
-                <Form.Control
-                  type="number"
-                  name="age"
-                  onChange={handleChange}
-                  value={values.age}
-                  isInvalid={!!errors.age && touched.age}
-                />
+                <Form.Label className="fw-bold">Idade</Form.Label>
+                <Form.Control type="number" name="age" onChange={handleChange} value={values.age} isInvalid={!!errors.age && touched.age} />
                 <Form.Control.Feedback type="invalid">{errors.age}</Form.Control.Feedback>
               </Form.Group>
             </Col>
@@ -178,7 +389,7 @@ const AdvertisementForm: React.FC = () => {
           <Row className="mb-3 g-3">
             <Col md="12">
               <Form.Group>
-                <Form.Label className='fw-bold'>Locais de atendimento</Form.Label>
+                <Form.Label className="fw-bold">Locais de atendimento</Form.Label>
                 {['Casa', 'Consultório', 'Meu local'].map((option) => (
                   <Form.Check
                     key={option}
@@ -194,7 +405,7 @@ const AdvertisementForm: React.FC = () => {
             </Col>
             <Col md="12">
               <Form.Group>
-                <Form.Label className='fw-bold'>Categoria</Form.Label>
+                <Form.Label className="fw-bold">Categoria</Form.Label>
                 {['Homem', 'Mulher', 'Crianças'].map((option) => (
                   <Form.Check
                     key={option}
@@ -210,7 +421,7 @@ const AdvertisementForm: React.FC = () => {
             </Col>
             <Col md="12">
               <Form.Group>
-                <Form.Label className='fw-bold'>Público</Form.Label>
+                <Form.Label className="fw-bold">Público</Form.Label>
                 {['Homens', 'Mulheres', 'Casal'].map((option) => (
                   <Form.Check
                     key={option}
@@ -226,7 +437,7 @@ const AdvertisementForm: React.FC = () => {
             </Col>
             <Col md="12">
               <Form.Group>
-                <Form.Label className='fw-bold'>Traços</Form.Label>
+                <Form.Label className="fw-bold">Traços</Form.Label>
                 {['L(o)', 'M(o)', 'R(o)', 'M(o)', 'J(o)'].map((option) => (
                   <Form.Check
                     key={option}
@@ -242,7 +453,7 @@ const AdvertisementForm: React.FC = () => {
             </Col>
           </Row>
 
-          <div className='text-center'>
+          <div className="text-center">
             <Button type="submit">Salvar anúncio</Button>
           </div>
         </Form>
