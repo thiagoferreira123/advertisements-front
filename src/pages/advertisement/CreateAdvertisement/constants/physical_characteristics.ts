89b92c0d76e2689705e478f0009c3723620bb544
Thiago Ferreira <nutricionistathiagoferreira@gmail.com>
2024-11-26 13:49:39 -0300

    a

--- src/pages/advertisement/CreateAdvertisement/constants/physical_characteristics.ts
+++ src/pages/advertisement/CreateAdvertisement/constants/physical_characteristics.ts
@@ -1,110 +1,104 @@
 export const physical_characteristics = [{
-    "Características físicas": {
-      "Raça": [
-        "Labrador",
-        "Pastor Alemão",
-        "Poodle",
-        "Bulldog",
-        "Outro"
-      ],
-      "Tamanho": [
-        "Pequeno",
-        "Médio",
-        "Grande",
-        "Gigante"
-      ],
-      "Pelagem": [
-        "Curta",
-        "Longa",
-        "Ondulada",
-        "Lisa",
-        "Densa"
-      ],
-      "Cor da Pelagem": [
-        "Marrom",
-        "Preto",
-        "Branco",
-        "Dourado",
-        "Manchado"
-      ],
-      "Idade": [
-        "Filhote",
-        "Adulto",
-        "Idoso"
-      ],
-      "Peso": [
-        "5 kg",
-        "15 kg",
-        "25 kg",
-        "40 kg"
-      ],
-      "Altura": [
-        "30 cm",
-        "50 cm",
-        "70 cm",
-        "90 cm"
-      ],
-      "Cor dos Olhos": [
-        "Castanho",
-        "Azul",
-        "Verde",
-        "Âmbar",
-        "Preto"
-      ],
-      "Nível de Energia": [
-        "Baixa",
-        "Moderada",
-        "Alta",
-        "Muito Alta"
-      ],
-      "Temperamento": [
-        "Amigável",
-        "Protetor",
-        "Tímido",
-        "Independente",
-        "Brincalhão"
-      ],
-      "Adestramento": [
-        "Sim",
-        "Não",
-        "Parcial"
-      ],
-      "Compatibilidade com Crianças": [
-        "Alta",
-        "Média",
-        "Baixa",
-        "Não Compatível"
-      ],
-      "Compatibilidade com Outros Animais": [
-        "Alta",
-        "Média",
-        "Baixa",
-        "Não Compatível"
-      ],
-      "Necessidade de Atividade Física": [
-        "Baixa",
-        "Média",
-        "Alta",
-        "Muito Alta"
-      ],
-      "Habilidades Especiais": [
-        "Busca",
-        "Guarda",
-        "Pastor",
-        "Trabalho",
-        "Companhia"
-      ],
-      "Frequência de Latidos": [
-        "Quase Nunca",
-        "Pouco",
-        "Moderado",
-        "Muito"
-      ],
-      "Necessidades de Higiene": [
-        "Baixa",
-        "Média",
-        "Alta",
-        "Muito Alta"
-      ]
-    }
+  "Características físicas": {
+    "Raça": [
+      "Labrador",
+      "Pastor Alemão",
+      "Poodle",
+      "Bulldog",
+      "Outro"
+    ],
+    "Tamanho": [
+      "Pequeno",
+      "Médio",
+      "Grande",
+      "Gigante"
+    ],
+    "Pelagem": [
+      "Curta",
+      "Longa",
+      "Ondulada",
+      "Lisa",
+      "Densa"
+    ],
+    "Cor da Pelagem": [
+      "Marrom",
+      "Preto",
+      "Branco",
+      "Dourado",
+      "Manchado"
+    ],
+    "Idade": [
+      "Filhote",
+      "Adulto",
+      "Idoso"
+    ],
+    "Peso": [
+      "",
+    ],
+    "Altura": [
+      "",
+    ],
+    "Cor dos Olhos": [
+      "Castanho",
+      "Azul",
+      "Verde",
+      "Âmbar",
+      "Preto"
+    ],
+    "Nível de Energia": [
+      "Baixa",
+      "Moderada",
+      "Alta",
+      "Muito Alta"
+    ],
+    "Temperamento": [
+      "Amigável",
+      "Protetor",
+      "Tímido",
+      "Independente",
+      "Brincalhão"
+    ],
+    "Adestramento": [
+      "Sim",
+      "Não",
+      "Parcial"
+    ],
+    "Compatibilidade com Crianças": [
+      "Alta",
+      "Média",
+      "Baixa",
+      "Não Compatível"
+    ],
+    "Compatibilidade com Outros Animais": [
+      "Alta",
+      "Média",
+      "Baixa",
+      "Não Compatível"
+    ],
+    "Necessidade de Atividade Física": [
+      "Baixa",
+      "Média",
+      "Alta",
+      "Muito Alta"
+    ],
+    "Habilidades Especiais": [
+      "Busca",
+      "Guarda",
+      "Pastor",
+      "Trabalho",
+      "Companhia"
+    ],
+    "Frequência de Latidos": [
+      "Quase Nunca",
+      "Pouco",
+      "Moderado",
+      "Muito"
+    ],
+    "Necessidades de Higiene": [
+      "Baixa",
+      "Média",
+      "Alta",
+      "Muito Alta"
+    ]
+  }
 }];
---
+++ src/pages/advertisement/CreateAdvertisement/constants/working_hours.ts
@@ -0,0 +1,33 @@
+export const working_hours = [{
+    "Horarios de expediente": {
+      "segunda-feira": {
+        "inicio": "",
+        "fim": ""
+      },
+      "terca-feira": {
+        "inicio": "",
+        "fim": ""
+      },
+      "quarta-feira": {
+        "inicio": "",
+        "fim": ""
+      },
+      "quinta-feira": {
+        "inicio": "",
+        "fim": ""
+      },
+      "sexta-feira": {
+        "inicio": "",
+        "fim": ""
+      },
+      "sabado": {
+        "inicio": "",
+        "fim": ""
+      },
+      "domingo": {
+        "inicio": "",
+        "fim": ""
+      }
+    }
+}];
+  
